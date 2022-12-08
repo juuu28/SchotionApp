1c0ae3680ab66a0a1cd513aeb5fb9838f58a6cc3
juuu28 <m.dajumafenori@gmail.com>
2022-12-08 20:27:57 +0700

    Progress 6

---
+++ app/src/main/java/com/example/schotion/onboarding1.kt
@@ -0,0 +1,28 @@
+package com.example.schotion
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class onboarding1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onboarding1)
+        hideActionBar()
+
+        val tv:TextView=findViewById(R.id.tv_OnNext)
+        val tv1:TextView=findViewById(R.id.tv_OnSkip)
+
+        tv.setOnClickListener{
+            startActivity(Intent(this,onboarding2::class.java))
+        }
+
+        tv1.setOnClickListener{
+            startActivity(Intent(this,MainActivity::class.java))
+        }
+    }
+    private fun hideActionBar() {
+        supportActionBar?.hide()
+        }
+}
